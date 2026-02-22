3562df4747d475ef7da64882911f63763938c15f
sam-thomas-dev <samsjournies@gmail.com>
2026-02-22 17:52:55 +1000

    model predict added

--- sky_seg/main.py
+++ sky_seg/main.py
@@ -7,24 +7,31 @@ import nn_definition
 import dataset_tools
 import model_training_testing
 
-learning_rate = 1e-3
-batch_size = 4
-epochs = 5
+# learning_rate = 1e-3
+# batch_size = 4
+# epochs = 5
 
-modelInstance = nn_definition.SegmentationModel()
-lossFunction = nn.BCEWithLogitsLoss()
-optimiser = torch.optim.Adam(modelInstance.parameters(), lr=learning_rate)
+# modelInstance = nn_definition.SegmentationModel()
+# lossFunction = nn.BCEWithLogitsLoss()
+# optimiser = torch.optim.Adam(modelInstance.parameters(), lr=learning_rate)
 
-# images, masks = next(iter(valLoader))
-# print(f"images: {images.shape}, {images.dtype}")
-# print(f"masks: {masks.shape}, {masks.dtype}")
+# # images, masks = next(iter(valLoader))
+# # print(f"images: {images.shape}, {images.dtype}")
+# # print(f"masks: {masks.shape}, {masks.dtype}")
 
-# trains model on entire dataset X times
-for iter in range(epochs):
-    print(f"Epoch: {iter}\n")
-    model_training_testing.train_loop(dataset_tools.trainLoader, modelInstance, lossFunction, optimiser, batch_size)
-    model_training_testing.test_loop(dataset_tools.valLoader, modelInstance, batch_size)
-    print(f"----------\n")
+# # trains model on entire dataset X times
+# for iter in range(epochs):
+#     print(f"Epoch: {iter}\n")
+#     model_training_testing.train_loop(dataset_tools.trainLoader, modelInstance, lossFunction, optimiser, batch_size)
+#     model_training_testing.test_loop(dataset_tools.valLoader, modelInstance, batch_size)
+#     print(f"----------\n")
 
-# saves only model weights & parameters
-torch.save(modelInstance.state_dict(), "./sky_seg/model_params.pt")
+# # saves only model weights & parameters
+# torch.save(modelInstance.state_dict(), "./sky_seg/model_params.pt")
+
+# test trained model
+model_inst = nn_definition.SegmentationModel()
+model_inst.load_state_dict(torch.load("./model_params.pt", weights_only=True))
+model_inst.eval
+
+model_training_testing.loadTestPrediction(dataset_tools.valLoader, model_inst, 4)
